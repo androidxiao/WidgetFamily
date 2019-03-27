e5162d65cd9f1e3b26bbe12e8d0f050649d89d72
androidxiao <zhawei1993@163.com>
2019-03-27 21:03:32 +0800

    添加 x_enableColor 方法

--- app/src/main/java/com/widget/family/ui/UIActivity.java
+++ app/src/main/java/com/widget/family/ui/UIActivity.java
@@ -2,7 +2,6 @@ package com.widget.family.ui;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 
 import com.widget.family.R;
